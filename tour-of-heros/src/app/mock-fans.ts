c00033e99fb7c13b8addadbc8cca58d05a29787b
thamaraiEd <62419000+thamaraiEd@users.noreply.github.com>
2021-02-14 20:50:29 +0530

    Add files via upload

---
+++ tour-of-heros/src/app/mock-fans.ts
@@ -0,0 +1,13 @@
+import { Fans } from './fans';
+export const Fanss: Fans[] = [
+     { id: 1, name: 'Rohan' },
+     { id: 2, name: 'BK' },
+    { id: 3, name: 'Makesh' },
+    { id: 4, name: 'Satish' },
+    { id: 5, name: 'Lotus' },
+    { id: 6, name: 'ZIg zag' },
+    { id: 7, name: 'BASHA' }
+    // {name: 'AABBCC'},
+   // {name: 'ZZZ'},
+    // {name: 'TTT'}
+  ];
